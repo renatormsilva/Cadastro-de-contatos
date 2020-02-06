2a220ba4503d666aa4ad4ff87bee2fb7a7e903ab
Renato Rodrigues <55606615+renatodln123@users.noreply.github.com>
2020-02-05 21:38:23 -0400

    Cadastro de contatos

---
+++ pesquisa.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="css/estilo.css" rel="stylesheet">
+
+    <title>Pesquisar</title>
+  </head>
+  <body>
+
+    <!-- Script PHP para a busca dos dados no banco de dados - Barra de pesquisa -->
+    
+    <?php 
+      // receber dados da barra de busca //armazenar
+      // executar uma ação
+      // metodo post
+      
+      $pesquisa = $_POST['busca'] ?? '';
+     
+      include "conexao.php";
+      
+      // Buscar dados do banco de dados
+      ## <!-- LIKE trecho da palavra -->
+      // Método de pesquisa 
+      // atributo
+
+      $sql = "SELECT * FROM pessoas WHERE nome LIKE '%$pesquisa%'";
+      // variável vai Receber os objetos lá do banco 
+      // criar um vetor na variável
+      // params de conexão e o  a instrução (vetor / dados)
+      $dados = mysqli_query($conn, $sql);
+
+      ?>
+    <!-- COMEÇO DO PROJETO PESQUISA - FORMULÁRIO DE CADASTRO -->
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>Pesquisar contatos</h1>
+
+          <nav class="navbar navbar-light bg-light">
+            <form class="form-inline" action="pesquisa.php" method="POST">
+              <input class="form-control mr-sm-2" type="search" placeholder="Nome" aria-label="Search" name="busca" autofocus>
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar </button>
+            </form>
+          </nav>
+
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Foto</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Endereço</th>
+                <th scope="col">Telefone</th>
+                <th scope="col">Email</th>
+                <th scope="col">Data de Nascimento</th>
+                <th scope="col">Funções</th>
+              </tr>
+            </thead>
+            <tbody>
+
+                <?php
+
+                // função que percorre o vetor resultante
+                // atribuição de variável 
+                // elemento 
+
+                while ($linha =  mysqli_fetch_assoc($dados) ) {
+                  $cod_pessoa = $linha['cod_pessoa']; 
+                  $nome = $linha['nome'];
+                  $endereco = $linha['endereco'];
+                  $telefone = $linha['telefone'];
+                  $email = $linha['email'];
+                  $data_nascimento = $linha["data_nascimento"];
+                  $data_nascimento = mostra_data($data_nascimento); 
+                  $foto = $linha['foto'];
+                  if (!$foto == null) {
+                    $mostra_foto = "<img src='img/$foto' class='lista_foto'>";
+                  } else {
+                    $mostra_foto = '';
+                  }
+
+                  $foto = $linha['foto'];
+                  echo "<tr>
+                          <th>$mostra_foto</th>
+                          <th scope='row'>$nome</th>
+                          <td>$endereco</td>
+                          <td>$telefone</td>
+                          <td>$email</td>
+                          <td>$data_nascimento</td>
+                          <td width=150px>
+                              <a href='cadastro_edit.php?id=$cod_pessoa' class='btn btn-success btn-sm'>Editar</a>
+                              <a href='#' class='btn btn-danger btn-sm' data-toggle='modal' data-target='#confirma'
+                              onclick=" .'"' ."pegar_dados($cod_pessoa, '$nome')" .'"' .">Excluir</a>
+                          </td>
+
+                        </tr>";
+                        # onclick="pegar_dados($id, '$nome')" O SEGREDO ESTÁ AQUI ""-->
+                        # $id numérico e não precisa de aspas
+                        # $nome não numérico precisa de aspas 
+               }
+              ?>
+              
+              
+            </tbody>
+          </table>
+          
+          <a href="index.php" class="btn btn-info"> Voltar para o Início</a>
+        </div>
+      </div>
+    </div>
+  </div>
+               <!-- Modal - Ciaxa de diálogo de confirmação -->
+    <div class="modal fade" id="confirma" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Confirmação de exclusão</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+             <form action="excluir_script.php" method="POST">
+             <!-- script do nome da pessoa com javascript -->
+            <p>Deseja realmente excluir <b id="nome_pessoa">nome do pessoa</b>?</p>
+            
+          </div>
+          <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Não</button>
+              <input type="hidden" name="nome" id="nome_pessoa1" value="">
+              <input type="hidden" name="id" id="cod_pessoa" value="">
+              <input type="submit" class="btn btn-danger" value="sim">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+               <!-- Script para pegar Dados via JAVASCRIPT -->
+    <script type="text/javascript">
+      function pegar_dados(id, nome) {
+        document.getElementById('nome_pessoa').innerHTML = nome;
+        document.getElementById('nome_pessoa1').value = nome;
+        document.getElementById('cod_pessoa').value = id;
+      }
+    </script>
+
+    
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
